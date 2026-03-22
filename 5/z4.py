67cdcfdb86d2a495e39737d25a8e700f1e11571f
kirillpetrov363-afk <kirillpetrov363@gmail.com>
2026-03-22 18:06:12 +0300

    z4.py

--- 5/z4.py
+++ 5/z4.py
@@ -7,13 +7,11 @@ start = today - datetime.timedelta(days=5 * 365)
 
 dates = array.array('i')
 
-# 10 случайных дат
 for i in range(10):
     days = random.randint(0, (today - start).days)
     d = start + datetime.timedelta(days=days)
     dates.append(d.toordinal())
 
-# разница между соседними
 for i in range(len(dates) - 1):
     d1 = datetime.date.fromordinal(dates[i])
     d2 = datetime.date.fromordinal(dates[i + 1])
