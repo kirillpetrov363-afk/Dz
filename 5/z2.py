b9ebea7adc0673d65a79fbaf065e9145d2445f8d
KazusPop <kirillpetrov363@gmail.com>
2026-03-22 18:04:45 +0300

    1

---
+++ 5/z2.py
@@ -0,0 +1,18 @@
+import random
+import statistics
+import math
+
+numbers = []
+for i in range(100):
+    numbers.append(random.randint(1, 100))
+
+mean = statistics.mean(numbers)
+median = statistics.median(numbers)
+stdev = statistics.stdev(numbers)
+total_sum = sum(numbers)
+sqrt_sum = math.sqrt(total_sum)
+rounded_sqrt = round(sqrt_sum, 2)
+
+print("\n",f"Среднее: {round(mean, 2)}, Медиана: {median}, "
+      f"Стандартное отклонение: {round(stdev, 2)}, "
+      f"Корень из суммы: {rounded_sqrt}","\n")
